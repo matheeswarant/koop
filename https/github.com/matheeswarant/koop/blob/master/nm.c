5849e8b95501ca0722a573fa3a3ef968b2a6d99f
matheeswarant <35597371+matheeswarant@users.noreply.github.com>
2018-01-21 04:59:17 +0000

    Create nm.c

---
+++ https/github.com/matheeswarant/koop/blob/master/nm.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main()
+{
+ int num;
+ printf("\n Enter any three  digit number");
+ scanf("%d","%d","%d",&number,&number,&number);
+ original number=n;
+ if(n!=0)
+ {
+  result+="r*r*r";
+  sum=num+sum;
+  }
+  else
+  {
+   return 0;
+   }
