0c55e95e8f80d4a386dc69261499a055f98e37ee
matheeswarant <35597371+matheeswarant@users.noreply.github.com>
2018-01-21 04:16:40 +0000

    Create mf.c

---
+++ mf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+long long a,p,result;
+printf("\n enter the num");
+scanf("%d",&a);
+printf("\n enter the power val");
+scanf("%d",&p);
+result=pow(a,p);
+printf("%d",result);
+}
