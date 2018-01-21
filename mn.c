80cd0a1a6bd24554eecf9ba6b4247e53c227f3e8
matheeswarant <35597371+matheeswarant@users.noreply.github.com>
2018-01-21 06:35:04 +0000

    Create mn.c

---
+++ mn.c
@@ -0,0 +1,15 @@
+
+# include <stdio.h>
+int main() 
+{ 
+int i, d,res ;   
+printf("\nEnter the num") ; 
+scanf("%d",&d) ; 
+printf("\n The num are ") ; 
+for(i=1;i<=5 ; i++) 
+{
+res=d*i;
+printf("%d",res) ; 
+}
+return 0;
+}
