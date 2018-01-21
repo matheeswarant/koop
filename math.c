7e91ce7b0dc37a278d8a69456df0329b6d85b76b
matheeswarant <35597371+matheeswarant@users.noreply.github.com>
2018-01-21 04:45:00 +0000

    Create math.c

---
+++ math.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(void) 
+{
+	int n,n1,n2,m,c=0;
+	scanf("%d,%d",&n1,&n2);
+	for(n=n1;n<n2;++n)
+	{
+	for(m=2;m<=m/2;k++)
+	{
+		if(n%m==0)
+		{
+		c++;
+
+		}
+	}
+	if(c==0)
+	{
+		printf("%d",n);
+		
+	}
+	}
+	return 0;
+}
