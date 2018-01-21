446cf39734ee7e9329afe1811c12a6ffe916017a
matheeswarant <35597371+matheeswarant@users.noreply.github.com>
2018-01-21 04:38:38 +0000

    Create mt.v

---
+++ mt.v
@@ -0,0 +1,18 @@
+#include<stdio.h>
+main()
+{
+ int number;
+ printf("\n Enter the first number");
+ scanf("%d",first number);
+ printf("\n Enter the second number");
+ scanf("%d",second number);
+ for(i=first number;i<=last number;i++)
+ {
+  temp=i/2;
+  ]
+  if(temp=0)
+  { 
+   printf("\n",i);
+   }
+   return 0;
+}
