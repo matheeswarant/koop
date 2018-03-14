7d9a6339c2dafe0943efe57d168225cf5cb804e5
matheeswarant <35597371+matheeswarant@users.noreply.github.com>
2018-03-14 16:17:10 +0530

    Update 5.c

--- 4/5.c
+++ 4/5.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
-void main()
+int main()
 {
-int a,c=0,n,n1;
+int c=0,n,n1;
 printf("enter the values");
 scanf("%d",&n);
-while(n<10)
+while(n>0)
 {
 n1=n%10;
 c++;
 n=n/10;
 }
-printf(("%d",c);
+printf("%d",c);
 }
