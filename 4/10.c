8231878641ead5ec7c7ffe68e0d417a483aef3ec
matheeswarant <35597371+matheeswarant@users.noreply.github.com>
2018-03-16 16:11:37 +0530

    Create 10.c

---
+++ 4/10.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+	int n;
+	printf("enter the values");
+	scanf("%d",&n);
+	if(n%2==0)
+	{
+		printf("%d is power of 2",n);
+	}
+	else
+	{
+		printf("%d is not power of 2");
+	}
+}
