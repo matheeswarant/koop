929dc66c73d3519bd4ef3d826827634a7833a89f
matheeswarant <35597371+matheeswarant@users.noreply.github.com>
2018-03-19 16:38:37 +0530

    Create 4.c

---
+++ 5/4.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c,sum;
+    printf("enter the number");
+    scanf("%d%d%d",&a,&b,&c);
+    sum=a+b+c;
+    sum=sum-1;
+    printf("sum is %d",sum);
+    
+}
